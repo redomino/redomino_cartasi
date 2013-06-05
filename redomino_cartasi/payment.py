4ffc4fe458ec78e4a46489b3150e7944b9af034b
Davide Moro <davide.moro@redomino.com>
2013-06-05 14:59:29 +0200

    removed unusued imports

--- redomino_cartasi/payment.py
+++ redomino_cartasi/payment.py
@@ -1,25 +1,7 @@
 import urllib
 
-from django.conf import settings
-
 from lfs.plugins import PaymentMethodProcessor
 from lfs.plugins import PM_ORDER_IMMEDIATELY
-
-from redomino_cartasi.conf import MAC_KEY
-from redomino_cartasi.conf import TERMINAL_ID
-from redomino_cartasi.conf import ACTION_CODE
-from redomino_cartasi.conf import CURRENCY
-from redomino_cartasi.conf import LANGUAGE
-from redomino_cartasi.conf import NOTIFICATION_URL
-from redomino_cartasi.conf import RESULT_URL
-from redomino_cartasi.conf import ERROR_URL
-from redomino_cartasi.conf import ANNULMENT_URL
-from redomino_cartasi.conf import VERSION_CODE
-from redomino_cartasi.conf import MESSAGE_TYPE
-from redomino_cartasi.conf import CO_PLATFORM
-from redomino_cartasi.conf import VPOS_ACTION
-
-from redomino_cartasi.utils import get_mac
 
 
 class CartasiPaymentMethodProcessor(PaymentMethodProcessor):
@@ -37,8 +19,8 @@ class CartasiPaymentMethodProcessor(PaymentMethodProcessor):
         return PM_ORDER_IMMEDIATELY
 
     def get_pay_link(self):
-#        # cartasi requires a POST while LFS performs a GET
-#        # We redirect to a form that performs a POST to cartasi
+        # cartasi requires a POST while LFS performs a GET
+        # We redirect to a form that performs a POST to cartasi
 
         data = {
                 'ORDER_ID': self.order.id,
